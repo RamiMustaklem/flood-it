103af309e3bd4163801948dc3d1e0470ba080d93
Rami Mustaklem <ramimustaklem@gmail.com>
2021-05-03 22:05:16 +0300

    Add useful comments and make sure to create data directory if it doesn't exist

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import app from './app';
 import * as http from 'http';
 import logger from './config/logger';
-import { setBaseDir } from './utils/ioUtils';
+import { setBaseDir, makeDataDir } from './utils/ioUtils';
 import path from 'path';
 
+const currentAppPath = path.join(__dirname, '../');
 /**
  * Set the app base path to the root path of the application outside of dist folder
  * */
-setBaseDir(path.join(__dirname, '../'));
+setBaseDir(currentAppPath);
+
+/**
+ * Create the data directory if it doesn't exist
+ */
+makeDataDir(currentAppPath).then();
 
 /* TODO: Move this to environment variables */
+// keeping this in order to be easier to launch the app without creating a .env file elsewhere
 const port = 5000;
 
 let server: http.Server;
--- src/utils/ioUtils.ts
+++ src/utils/ioUtils.ts
@@ -1,11 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 const fsPromises = fs.promises;
+import logger from '../config/logger';
 
 let baseDir = '';
 
 type DataEntity = 'grid' | string; // not sure if we need something other than grid
 
+/**
+ * Saves the passed data to the json file
+ * @param entity
+ * @param data
+ */
 export const writeData = async (entity: DataEntity, data: any) => {
     try {
         return await fsPromises.writeFile(`${getDataDir()}${entity}.json`, JSON.stringify(data), 'utf8');
@@ -14,6 +20,10 @@ export const writeData = async (entity: DataEntity, data: any) => {
     }
 };
 
+/**
+ * Reads the saved json file to return the json data
+ * @param entity
+ */
 export const readData = async (entity: DataEntity) => {
     try {
         const gridData = await fsPromises.readFile(`${getDataDir()}${entity}.json`, 'utf8');
@@ -23,11 +33,33 @@ export const readData = async (entity: DataEntity) => {
     }
 };
 
-// TODO: create /data/ dir if it doesn't exist
+/**
+ * Create the data directory if it doesn't exist
+ * @param baseDir
+ */
+export const makeDataDir = async (baseDir: string) => {
+    // if the folder exists do not cause an app crash
+    try {
+        const getRootDirList = await fsPromises.readdir(`${baseDir}`);
+        if (!getRootDirList.includes('data')) {
+            return await fsPromises.mkdir(`${baseDir}data`);
+        }
+    } catch (e) {
+        logger.error('error creating data directory', e);
+    }
+};
+
+/**
+ * Set the application base directory
+ * @param __basedir
+ */
 export const setBaseDir = (__basedir: string) => {
     baseDir = __basedir;
 };
 
+/**
+ * Return the data directory
+ */
 export const getDataDir = () => {
     return path.join(baseDir, './data/');
 };
